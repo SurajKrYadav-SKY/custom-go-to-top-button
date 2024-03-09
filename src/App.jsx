82f3aa662de46fee122119560fcdaaf8e26d7006
Suraj Kumar Yadav <suraj.mails99@gmail.com>
2024-03-09 12:50:29 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css'
+import DummyData from './components/DummyData'
+import GoToTop from './components/to-the-top-button/GoToTop'
+
+function App() {
+
+  return (
+    <center>
+      <h1>These are just Dummy data</h1>
+      <DummyData/>
+      <DummyData/>
+
+      <GoToTop/>
+
+      <DummyData/>
+      <DummyData/>
+      <DummyData/>
+      <DummyData/>
+      <DummyData/>
+      <DummyData/>
+      <DummyData/>
+    </center>
+  )
+}
+
+export default App
---
+++ src/components/DummyData.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DummyData = () => {
+  return (
+    <div style={{margin : "20px 15% 0px 15%" }}>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aspernatur omnis quisquam, obcaecati veniam, perspiciatis maxime adipisci est amet alias blanditiis expedita, quia aliquam in ab vitae cupiditate ducimus pariatur nobis nulla accusamus deserunt. Perferendis, corporis voluptatum sunt mollitia, saepe aperiam totam officia culpa aliquam quaerat nihil soluta sint et quasi dolores? Numquam consectetur, quod reprehenderit, dicta nam veritatis, quos optio vitae laboriosam sed impedit pariatur facilis aspernatur adipisci quas tempore eaque molestias. Esse vero vitae consectetur, obcaecati omnis suscipit aperiam, sapiente cumque ipsam cupiditate ex nesciunt hic voluptatibus natus sunt voluptate aspernatur in nam! Inventore quam cupiditate perferendis quos ipsa laborum assumenda porro ad provident alias obcaecati, temporibus repellendus sed placeat, animi ipsum nulla adipisci aliquam facilis! Quam iste totam possimus inventore laudantium nobis eaque dolor libero blanditiis facilis! Itaque pariatur quia reprehenderit obcaecati? Facere incidunt vero quae reprehenderit est recusandae deleniti beatae laudantium, quos dicta! Inventore quaerat et laudantium repellendus commodi rerum velit consectetur? Quis velit eveniet assumenda ullam ducimus tempore consequuntur, optio culpa dignissimos debitis aspernatur suscipit ipsam veritatis magni nisi nobis repellat doloribus nemo voluptatum harum incidunt. Eaque incidunt doloremque a eum vel, sapiente amet corrupti porro atque dolorum, distinctio rem. Necessitatibus culpa totam dolor veritatis amet.
+    </div>
+  )
+}
+
+export default DummyData
